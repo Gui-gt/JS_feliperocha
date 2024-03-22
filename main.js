1ba280524a88c036ae90da02bb1b7be2cd3a7151
Gui-gt <30755guilherme@gmail.com>
2024-03-22 20:27:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,27 @@ console.log(`Meu nome é ${firstName.toLowerCase()} ${lastName.toUpperCase()}`)
 const names = 'Felipe,Guilherme,Maria'
 
 console.log(names.split(','))
+
+
+//Numbers
+const number = 5
+console.log(typeof number)
+console.log(typeof number.toString())
+
+//bolleans
+//true or false
+console.log(2 == 2)
+
+//Null and Undefined
+
+const x = null;
+const z = undefined;
+
+console.log(typeof x)
+
+//Arrays
+
+const list = [1,2,3,4,5,6];
+
+//Objects
+const object = {name: 'Felipe'}
