ef26c45d5a09e31ae8313660082501c7618f7170
Gui-gt <30755guilherme@gmail.com>
2024-03-22 20:47:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,8 @@ const names = ["Felipe","João","Julia",10,false];
 const joao = names[1]
 console.log(joao)
 
-names.push("Pedro");
+names.push("Pedro"); //add no final da lista
+names.unshift(20); // add no inicio da lista
+names.pop(); // remove da lista
+console.log(names) // imprime no console
 
-console.log(names)
-
-names.unshift(20);
-
-console.log(names)
