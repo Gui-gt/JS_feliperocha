0b9d8b5642ce40ff10b5a1eb1940b9afc20ca9dd
Gui-gt <30755guilherme@gmail.com>
2024-03-23 09:17:02 -0300

    att

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
-const names = ["Felipe","João","Julia",10,false];
 
-const joao = names[1]
-console.log(joao)
 
-names.push("Pedro"); //add no final da lista
-names.unshift(20); // add no inicio da lista
-names.pop(); // remove da lista
-console.log(names) // imprime no console
+const numbers = [1,2,3,4,5]
 
-names [3] = "Gustavo" // altera a constante
+console.log(numbers.map(function(number){
 
-console.log(names)
-const indexof = (names.indexOf('Gustavo'))
-
-const sortedNames = (names.sort())
-console.log(sortedNames)
-
-const namesIsArray = Array.isArray(names);
-console.log(namesIsArray);
-
+}))
--- main01.js
+++ main01.js
@@ -33,3 +33,24 @@ const list = [1,2,3,4,5,6];
 
 //Objects
 const object = {name: 'Felipe'}
+
+const names = ["Felipe","João","Julia",10,false];
+
+const joao = names[1]
+console.log(joao)
+
+names.push("Pedro"); //add no final da lista
+names.unshift(20); // add no inicio da lista
+names.pop(); // remove da lista
+console.log(names) // imprime no console
+
+names [3] = "Gustavo" // altera a constante
+
+console.log(names)
+const indexof = (names.indexOf('Gustavo'))
+
+const sortedNames = (names.sort())
+console.log(sortedNames)
+
+const namesIsArray = Array.isArray(names);
+console.log(namesIsArray);
