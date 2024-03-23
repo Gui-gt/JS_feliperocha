d8368a05ad74d1133d16c32ed2f023aad551e3eb
Gui-gt <30755guilherme@gmail.com>
2024-03-23 10:31:30 -0300

    att

--- main01.js
+++ main01.js
@@ -76,3 +76,25 @@ const evenAges = ages.filter(function(age){
 }); // filter pode separar numeros impares e pares
 
 console.log(evenAges);
+const ages = [12,22,30,15,17]
+
+const agesTwosomed = ages.map(function(number){
+    return number* 2
+})
+
+console.log(agesTwosomed)
+
+const number = [10,22,33]
+
+const evenNumbers = number.filter(function(number){
+    return number % 2 === 0 
+})
+console.log(evenNumbers)
+
+const idade = [10,12]
+
+const somaIdade = idade.reduce(function(idade, soma){
+    return idade + soma
+},100)
+
+console.log(somaIdade)
