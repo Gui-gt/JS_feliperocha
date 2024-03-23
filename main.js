7bb28de355a1d0f5ac48f2158327cc3b2eb3058a
Gui-gt <30755guilherme@gmail.com>
2024-03-23 18:37:06 -0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const person = [
+    {
+        Id:1,
+        name: 'Guilherme',
+        age: 20,
+        profissao: 'Desenvolvedor'
+    },
+    {
+        id:2,
+        name: 'Giovana',
+        age: 21,
+        profissao: 'Arquiteta'
+    }
+]
+
