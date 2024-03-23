9a9dddc7299b40324e9cbab9dc2ab18b6eae0de6
Gui-gt <30755guilherme@gmail.com>
2024-03-23 10:36:37 -0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const person = {
+    firstname: 'Guilherme',
+    lastName: 'Henrique',
+    age:'20',
+    hobbies: ['Ficar com a namorada','passear','estudar']
+}
+
+const firstName = person.firstname
+const lastName = person.lastName
+
+console.log(firstName)
+console.log(lastName)
