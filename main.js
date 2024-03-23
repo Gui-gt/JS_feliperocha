bdaf98f5b5cf2e88db9d6e96338ff0e61181ab2f
Gui-gt <30755guilherme@gmail.com>
2024-03-23 09:28:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-
-
-const numbers = [1,2,3,4,5]
+const numbers = [1,2,3,4,6]
 
 const numbersMultipliedbyTwo = numbers.map(function(number){
     return number * 2
 })
-
-console.log(numbers)
+console.log(numbersMultipliedbyTwo)
