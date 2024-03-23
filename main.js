ce98e371bb50487ae2ddf03e38175d204d6e1232
Gui-gt <30755guilherme@gmail.com>
2024-03-23 09:43:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ const numbersMultipliedbyTwo = numbers.map(function(number){
     return number * 2
 })
 console.log(numbersMultipliedbyTwo)
+
+const ages = [4,11,7,16,40]
+
+//const evenAges = ages.filter(function(age){
+    return age % 2 === 0
+})
+
+console.log(evenAges)
