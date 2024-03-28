566e77d280380ebc29f92a93ceb5400311fbafb6
Gui-gt <30755guilherme@gmail.com>
2024-03-28 09:47:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-const a = 5
+//condicional
+//const sum1 = 1 + 1
+//const sum2 = 2 + 2
+//if(sum1 === 2 && sum2 === 4){
+//    console.log(`sum1 is true and sum2 is true`)
+//}else if(sum1 != 2 || sum2 != 4){
+//    console.log(`error`)
+//}
+//repetição
 
-const b = 2
+//array/Lista
 
-if(a < b){
-    console.log('sim')
-}else{
-    console.log(nao)
+for(let c = 0;c <= 10;c++){
+    console.log(c)
 }
-
