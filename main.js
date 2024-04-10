5d97bc1f14fe1f8e2bed87bf984db59947a2f9b1
Gui-gt <30755guilherme@gmail.com>
2024-04-10 19:00:16 -0300

    att

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const numbers = [1,2,3,4,5]
+const numbers = [1,2,3,4,5];
 
-const numberseven = numbers.filter(function(numbers){
-    return numbers % 2 == 0
-});
-console.log(numberseven);
+const numberSum = numbers.reduce(function(numbers, acumulator){
+    return numbers + acumulator
+})
+
+console.log(numberSum);
