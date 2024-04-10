691878105f6a0321b3f986a081d17450b375f2e9
Gui-gt <30755guilherme@gmail.com>
2024-04-10 18:58:19 -0300

    att

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const numbers = [1,2,3,4,5]
 
-const numberxTwo = numbers.map(function(numbers){
-    return numbers * 2;
+const numberseven = numbers.filter(function(numbers){
+    return numbers % 2 == 0
 });
-
-console.log(numberxTwo);
+console.log(numberseven);
